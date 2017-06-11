290d78ab685a6c6bc5a5fb663b31760b9bcc6a6c
naooooo <g15949tn@gm.tsuda.ac.jp>
2017-06-11 15:57:55 +0900

    画面間でのアルバイトのデータの受け渡し可能

--- app/src/main/java/com/example/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.calendarapp.R.id.datePicker;
 
+public class MainActivity extends AppCompatActivity {
+    Calendar cal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
     }
     public void goToDaily(View view){
         Intent intent=new Intent(this,DailyActivity.class);
+        DatePicker dp = (DatePicker)findViewById(R.id.datePicker);
+        int year = dp.getYear();//年を取得
+        int month = dp.getMonth();//月を取得
+        int day = dp.getDayOfMonth();//日を取得
+        intent.putExtra("DAY",day);
+        intent.putExtra("MONTH",month+1);
+        intent.putExtra("YEAR",year);
         startActivity(intent);
+    }
+    public void receivedData(View view){
+        Intent intent=getIntent();
+        Calendar cal = (Calendar) intent.getSerializableExtra("Cal");
+        System.out.println("" + cal.getYear() + "年" + cal.getMonth() + "月" + cal.getDay() + "日" + cal.getSh() + "：" + cal.getSm() + "〜" + cal.getFh() + "：" + cal.getFm());
+    }
+    public void cal(View view) {
+
     }
 }
